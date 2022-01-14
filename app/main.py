8fa11c2ac17cb7d20997d44eb2cfe482c3f1eab7
Sasha Tsepilova <oleksandra.tsepilova@ucu.edu.ua>
2022-01-14 22:58:56 +0200

    Bubuntics server

--- app/main.py
+++ app/main.py
@@ -4,6 +4,11 @@ from flask import request
 app = Flask(__name__)
 
 last_info = "There were no info yet"
+operation = 0
+with open("app/operation.txt", 'r') as fin:
+    operation = fin.readline()
+    operation = operation.rstrip()
+
 def change_info(inf):
     global last_info 
     last_info= inf
@@ -11,14 +16,25 @@ def change_info(inf):
 @app.route("/", methods = ['POST', 'GET'])
 def new_info():
     global last_info
+    global operation
     if request.method == 'POST':
-        last_info= request.args.get("info",'')
-    return last_info
+        if request.form['switch_option'] == 'Do Something':
+            if operation == '1':
+                operation = '2'
+            else:
+                operation = '1'
+            with open("app/operation.txt", 'w') as fout:
+                fout.write(operation)
+        
+        else:
+            last_info= request.args.get("info",'')
+    return last_info+"""<form method="post"><input type="submit" name="switch_option" value="Do Something"></form>"""
 
 
 
 @app.route("/operation", methods = ['GET'])
-def operation():  
-    return "2"
+def operation(): 
+    global operation 
+    return operation
 
 
