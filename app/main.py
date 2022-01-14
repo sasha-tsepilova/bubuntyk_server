084aeb82dbf8683bdc981d6f0d775baf459d7f61
Sasha Tsepilova <oleksandra.tsepilova@ucu.edu.ua>
2022-01-14 14:47:08 +0200

    Bubuntics server

---
+++ app/main.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+last_info = "There were no info yet"
+def change_info(inf):
+    global last_info 
+    last_info= inf
+
+@app.route("/", methods = ['POST', 'GET'])
+def new_info():
+    global last_info
+    if request.method == 'POST':
+        last_info= request.args.get("info",'')
+    return last_info
+
+
+
