ef630c7db502252039b45cc87b2aa53dbc1f2057
Sasha Tsepilova <oleksandra.tsepilova@ucu.edu.ua>
2022-01-15 11:10:16 +0200

    post fixes

--- app/main.py
+++ app/main.py
@@ -16,6 +16,13 @@ def change_info(inf):
 @app.route("/", methods = ['POST', 'GET'])
 def new_info():
     global last_info
+    if request.method == 'POST':
+        last_info= request.args.get("info",'')
+    return last_info
+
+
+@app.route("/button", methods =['POST', 'GET'])
+def button():
     global operation
     if request.method == 'POST':
         if request.form['switch_option'] == 'Do Something':
@@ -25,12 +32,7 @@ def new_info():
                 operation = '1'
             with open("app/operation.txt", 'w') as fout:
                 fout.write(operation)
-        
-        else:
-            last_info= request.args.get("info",'')
-    return last_info+"""<form method="post"><input type="submit" name="switch_option" value="Do Something"></form>"""
-
-
+    return """<form method="post"><input type="submit" name="switch_option" value="Do Something"></form>"""
 
 @app.route("/operation", methods = ['GET'])
 def operate(): 
